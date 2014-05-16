b1ba3142f994ddeddfb74b3dd4426e457e98588f
alsotang <alsotang@gmail.com>
2014-05-16 12:34:03 +0800

    `emit`

--- test/eventr.test.js
+++ test/eventr.test.js
@@ -1,13 +1,14 @@
 var Eventr = require('../');
 
 var fetchurl = function (url, callback) {
-  var page = 'page ' + url;
+  var page = 'pagecontent' + url;
   setTimeout(function () {
     callback(null, page);
   }, 5);
 };
 
 describe('eventr.js', function () {
+
   it('should work with one event', function () {
     var et = new Eventr();
     et.on('hello', function (edata) {
@@ -25,9 +26,28 @@ describe('eventr.js', function () {
       et.emit('page2', content);
     });
     et.on(['page1', 'page2'], function (edata) {
-      edata.page1.should.equal('page 1');
-      edata.page2.should.equal('page 2');
+      edata.page1.should.equal('pagecontent1');
+      edata.page2.should.equal('pagecontent2');
       done();
+    });
+  });
+
+  it('should work with multi emit', function () {
+    var et = new Eventr();
+
+    fetchurl('1', function (err, content) {
+      et.emit('1', content);
+    });
+    fetchurl('2', function (err, content) {
+      et.emit('2', content);
+    });
+
+    et.on('page1', function (edata) {
+      edata.should.equal('pagecontent1');
+    });
+    et.on(['page1', 'page2'], function (edata) {
+      edata.page1.should.equal('pagecontent1');
+      edata.page2.should.equal('pagecontent2');
     });
   });
 });
